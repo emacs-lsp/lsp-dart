8180a46ea8699c461a49904432492c7e4e1f6df8
Eric Dallo <ericdallo06@hotmail.com>
2020-12-06 14:25:31 -0300

    Bump lsp-dart: 1.17.11

--- lsp-dart.el
+++ lsp-dart.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2020 Eric Dallo
 
-;; Version: 1.17.10
+;; Version: 1.17.11
 ;; Package-Requires: ((emacs "26.1") (lsp-treemacs "0.3") (lsp-mode "7.0.1") (dap-mode "0.6") (f "0.20.0") (dash "2.14.1") (pkg-info "0.4") (dart-mode "1.0.5"))
 ;; Keywords: languages, extensions
 ;; URL: https://emacs-lsp.github.io/lsp-dart
