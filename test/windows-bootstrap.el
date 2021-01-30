9b0840ad03aeca2c5ad51a5e51fb003463988817
JenChieh <jcs090218@gmail.com>
2021-01-30 21:00:39 +0800

    Try resolve bad request

--- test/windows-bootstrap.el
+++ test/windows-bootstrap.el
@@ -29,7 +29,7 @@
                                          user-emacs-directory))
 
 (let* ((package-archives '(("melpa" . "https://melpa.org/packages/")
-                           ("gnu" . "http://elpa.gnu.org/packages/")))
+                           ("gnu" . "https://elpa.gnu.org/packages/")))
        (pkgs (append '(lsp-treemacs lsp-mode dap-mode f dash pkg-info dart-mode)
                      '(el-mock ert-runner undercover spinner))))
   (package-initialize)
